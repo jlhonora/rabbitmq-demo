930cccc5d639e92955c24c123a6febfb719331ab
jlhonora <jlhonora@ing.puc.cl>
2015-07-16 11:19:06 -0300

    Add run/kill for all clients

--- kill.sh
+++ kill.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-kill -TERM `ps aux | grep 'ruby client' | awk -F ' ' '{print $2}'`
-kill -TERM `ps aux | grep 'ruby server' | awk -F ' ' '{print $2}'`
+kill -TERM `ps aux | grep 'ruby client'   | awk -F ' ' '{print $2}'`
+kill -TERM `ps aux | grep 'ruby server'   | awk -F ' ' '{print $2}'`
+kill -TERM `ps aux | grep 'python client' | awk -F ' ' '{print $2}'`
+kill -TERM `ps aux | grep 'go-client'     | awk -F ' ' '{print $2}'`
--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+echo "Starting ruby server"
 ruby server.rb &
+
+echo "Starting ruby client"
 ruby client.rb &
+
+echo "Starting go client"
+go build -o go-client client.go && ./go-client &
+
+echo "Starting python client"
+python client.py &
