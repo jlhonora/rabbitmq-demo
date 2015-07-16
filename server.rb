3d096a8ed103ddcf1e4194743cb6187f09e0deed
jlhonora <jlhonora@ing.puc.cl>
2015-07-16 12:29:25 -0300

    Setting default values for queue

--- server.rb
+++ server.rb
@@ -7,20 +7,36 @@ def get_timestamp
   "#{Time.now.strftime('%H:%M:%S')}"
 end
 
-conn = Bunny.new(hostname: "amqp://guest:guest@localhost:5672/")
+conn = Bunny.new
 conn.start
 
 queue_name = ENV['RABBITMQ_QUEUE'] || 'default_queue'
 
-ch   = conn.create_channel
-q    = ch.queue(queue_name)
+ch = conn.create_channel
+
+# Messages only live for 5 seconds. This
+# can be configured as an installation policy,
+# check https://www.rabbitmq.com/ttl.html
+args = {}
+args['x-message-ttl'] = 5000
+
+q = ch.queue(
+  queue_name,
+  :arguments   => args,
+  :auto_delete => false,  # The queue won't be deleted when there are no more consumers.
+  :exclusive   => false,  # Exclusive queues may only be accessed by the current
+                          # connection, and are deleted when that connection closes.
+  :durable     => true    # The queue doesn't die if RabbitMQ restarts
+)
 
 seq = 0
 
 begin
   while true
     msg = "#{seq}"
-    ch.default_exchange.publish(msg, :routing_key => q.name)
+    ch.default_exchange.publish(msg,
+                                :routing_key => q.name,
+                                :persistent  => true) # Save the message to disk
     puts "[#{get_timestamp}] [server] Sent '#{msg}'"
     seq += 1
 
